e9ef2c91908063590aa31aa6f035fd2df8c9360c
gercova <germancotrina17@gmail.com>
2026-03-28 17:46:36 -0500

    refactor: implementación de nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,36 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
 
 Route::get('/',         [AppController::class, 'index'])->name('inicio');
 Route::get('/ofertas',  [AppController::class, 'offers'])->name('ofertas');
 Route::get('/nosotros', [AppController::class, 'aboutus'])->name('nosotros');
+
+Route::get('/register',                     [RegisteredUserController::class, 'create'])->name('register');
+Route::post('/register',                    [RegisteredUserController::class, 'store']);
+Route::get('/login',                        [AuthenticatedSessionController::class, 'create'])->name('login')->middleware('guest');
+Route::post('/login',                       [AuthenticatedSessionController::class, 'login'])->middleware('guest');
+Route::post('/logout',                      [AuthenticatedSessionController::class, 'logout'])->name('logout');
+Route::get('forgot-password',               [NewPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('forgot-password',              [NewPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+// Restablecer contraseña
+Route::get('reset-password/{token}',        [PasswordResetLinkController::class, 'showResetForm'])->name('password.reset');
+Route::post('reset-password',               [PasswordResetLinkController::class, 'reset'])->name('password.update');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard',    [DashboardController::class, 'index'])->name('dashboard');
+    
+    Route::get('/profile',      [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile',    [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile',   [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';

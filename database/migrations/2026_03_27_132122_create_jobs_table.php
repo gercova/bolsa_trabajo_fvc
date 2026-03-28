9d6e5ca86100faf769ccecbd97997c46a1c3b60c
gercova <germancotrina17@gmail.com>
2026-03-28 17:45:30 -0500

    feat: implementación de migraciones

---
+++ database/migrations/2026_03_27_132122_create_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('company');
+            $table->string('location')->nullable();
+            $table->text('description')->nullable();
+            $table->string('url')->unique(); // Vital para evitar duplicados
+            $table->string('source'); // Para saber de qué página lo sacamos
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_offers');
+    }
+};

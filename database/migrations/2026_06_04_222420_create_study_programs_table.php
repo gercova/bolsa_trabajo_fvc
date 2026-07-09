870cd2ba53b8f5b9d2d5d437e2565d943d7ff555
gercova <germancotrina17@gmail.com>
2026-07-09 00:48:02 -0500

    feat: implementación del campo slug

--- database/migrations/2026_06_04_222420_create_study_programs_table.php
+++ database/migrations/2026_06_04_222420_create_study_programs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('study_programs', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->string('logo_path')->nullable();
             $table->text('description');
             $table->text('details');

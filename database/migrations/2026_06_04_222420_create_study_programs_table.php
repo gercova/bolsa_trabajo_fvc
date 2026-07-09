ade01e6747afbd45417f17900c0d20e26f0dfc1f
gercova <germancotrina17@gmail.com>
2026-07-08 21:08:36 -0500

    feat: implementación de nuevas migraciones de ubigeo, programas de estudio, blogs, modulos de estudio y colaboradores

---
+++ database/migrations/2026_06_04_222420_create_study_programs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('study_programs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('logo_path');
+            $table->text('description');
+            $table->text('details');
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('study_programs');
+    }
+};
---
+++ database/migrations/2026_07_08_085555_create_collaborators_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collaborators', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('program_id')->nullable();
+            $table->foreign('program_id')->references('id')->on('study_programs')->onDelete('cascade');
+            $table->boolean('is_coordinator')->default(0);
+            $table->boolean('is_administrative')->default(0);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collaborators');
+    }
+};

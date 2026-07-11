5064e90068fb5d09b0df6a1e7297196cc487343d
gercova <germancotrina17@gmail.com>
2026-07-11 02:22:37 -0500

    feat: test de usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'names' => fake()->name(),
+            'dni' => fake()->unique()->numerify('########'),
             'email' => fake()->unique()->safeEmail(),
+            'role' => 'Admin',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),

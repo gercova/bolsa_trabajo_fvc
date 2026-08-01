44e37a3043b23c6d160628f8612811d942738c8a
gercova <germancotrina17@gmail.com>
2026-08-01 12:55:22 -0500

    feat: clase de UserRoleDetailsSeeder agregado a las seeder de DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AdmissionRequirementSeeder::class);
         $this->call(PartnersSeeder::class);
         $this->call(TupaSeeder::class);
+        $this->call(UserRoleDetailsSeeder::class);
     }
 }
 

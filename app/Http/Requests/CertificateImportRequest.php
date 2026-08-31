2c280b7424ae349728e4166b30db6413e4443794
gercova <germancotrina17@gmail.com>
2026-08-31 00:35:37 -0500

    feat: clase de CertificateImportRequest añadido

---
+++ app/Http/Requests/CertificateImportRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CertificateImportRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'file' => ['required', 'file', 'mimes:xlsx,xls,csv', 'max:10240'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'file.required' => 'Debe seleccionar un archivo para importar.',
+            'file.file'     => 'El campo debe ser un archivo válido.',
+            'file.mimes'    => 'Solo se permiten archivos Excel (.xlsx, .xls) o CSV (.csv).',
+            'file.max'      => 'El archivo no debe superar los 10 MB.',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'file' => 'archivo de certificados',
+        ];
+    }
+}
